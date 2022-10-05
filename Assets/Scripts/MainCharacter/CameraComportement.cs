8c6fb41c7b4884db473a789b72172d9506fae328
RustyGithubb <98985426+RustyGithubb@users.noreply.github.com>
2022-10-05 15:37:49 +0200

    Cube StartingBlock #1

--- Assets/Scripts/MainCharacter/CameraComportement.cs
+++ Assets/Scripts/MainCharacter/CameraComportement.cs
@@ -1,25 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class CameraComportement : MonoBehaviour
 {
 
     public GameObject OwnCharacter;
-    public float _Lag, _LagRotation;
+    public float _Lag, _LagRotation, _DistanceLag;
     Vector3 Direction;
     Quaternion DirectionQuat;
 
     private void FixedUpdate()
     {
-        Direction = OwnCharacter.GetComponent<Rigidbody>().velocity;
-        DirectionQuat = Quaternion.Euler(Direction);
+        Direction = OwnCharacter.GetComponent<CharacterController>().ForwardVelocity.normalized;
+        ;
+        print(Direction);
 
+        transform.parent.transform.forward  = Vector3.Lerp(transform.parent.transform.forward, Direction, _LagRotation * Time.deltaTime);
 
-        transform.localPosition = Vector3.Lerp(transform.localPosition, OwnCharacter.transform.position, Time.deltaTime * _Lag);
+        transform.parent.transform.position = Vector3.Lerp(transform.parent.transform.position, OwnCharacter.transform.position, Time.deltaTime * _Lag);
 
         
-        transform.localRotation = Quaternion.Lerp(transform.localRotation, DirectionQuat, Time.deltaTime * _LagRotation);
     }
 
 
--- Assets/Scripts/MainCharacter/CharacterController.cs
+++ Assets/Scripts/MainCharacter/CharacterController.cs
@@ -11,6 +11,8 @@ public class CharacterController : MonoBehaviour
     Vector2 MovementValue;
     public float Speed, _Mult, _DragMult;
     private float InitialSpeed;
+    public Vector3 ForwardVelocity;
+    public GameObject SupportCamera;
 
     //start =====================================
     private void Awake()
@@ -48,15 +50,23 @@ public class CharacterController : MonoBehaviour
     {
         if(MainController.Gamepad.Movement.ReadValue<Vector2>() != new Vector2(0,0))
         {
-            MovementValue.x = MainController.Gamepad.Movement.ReadValue<Vector2>().x;
-            MovementValue.y = MainController.Gamepad.Movement.ReadValue<Vector2>().y;
+            MovementValue.x = MainController.Gamepad.Movement.ReadValue<Vector2>().x ;
+            MovementValue.y = MainController.Gamepad.Movement.ReadValue<Vector2>().y ;
             Vector3 _Velocity = rb.velocity;
 
 
             _Velocity.z = Speed * MovementValue.y;
             _Velocity.x = Speed * MovementValue.x;
 
-            rb.velocity = _Velocity;
+            //rb.velocity = _Velocity;
+            Debug.Log(SupportCamera.transform.forward);
+            //rb.velocity = new Vector3(MovementValue.x * SupportCamera.transform.right.x,
+            //    0,
+            //    _Velocity.z * SupportCamera.transform.forward.z);
+            
+            rb.velocity = _Velocity.x * SupportCamera.transform.right + _Velocity.z * SupportCamera.transform.forward;
+            //rb.AddForce(SupportCamera.transform.right.z*MovementValue.x, 0, SupportCamera.transform.forward.z*MovementValue.y, ForceMode.Impulse);
+
             if (_Mult <= 3)
             {
                 _Mult += Time.deltaTime * _DragMult;
@@ -70,7 +80,8 @@ public class CharacterController : MonoBehaviour
             {
                 Speed = InitialSpeed * _Mult;
             }
-            
+
+            ForwardVelocity = rb.velocity;
 
         }
         else
