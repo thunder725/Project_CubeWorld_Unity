4d8e78954d63cfa962be5e65d0742f2e1a9c1d5c
thunder725 <leonard.loustalot@gmail.com>
2022-10-05 15:58:39 +0200

    Debut anim AllCoinChallenge

---
+++ Assets/Scripts/GeneralEventManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class GeneralEventManager : MonoBehaviour
+{
+
+    public static UnityEvent PauseGameplay;
+    public static UnityEvent ResumeGameplay;
+
+    
+}
