77937e36f1a9a0160da5b3f269cbcd18e9a1fd77
Melinette13 <97893291+Melinette13@users.noreply.github.com>
2022-10-07 12:56:10 +0200

    disco

---
+++ Assets/Scripts/GPEs/Discotek.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Discotek : MonoBehaviour
+{
+    
+     public float timer = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 2.0f)//change the float value here to change how long it takes to switch.
+        {
+            // pick a random color
+            Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+            // apply it on current object's material
+            GetComponent<Renderer>().material.color = newColor;
+            timer = 0;
+        }
+
+
+    }
+}
