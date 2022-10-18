8b1b57bd15e2022c3c29bb4d8c457f8bec0e17ec
thunder725 <lloustalot@artfx.fr>
2022-10-18 15:39:02 +0200

    re-re-updated billboard script

--- Assets/Scripts/BillboardScript.cs
+++ Assets/Scripts/BillboardScript.cs
@@ -4,6 +4,7 @@ public class BillboardScript : MonoBehaviour
 {
     public enum PotentialAxes { RedAxis, GreenAxis, BlueAxis };
     public PotentialAxes billboardAxis;
+    public bool invertAxis;
 
 
     public bool isBillboardActive;
@@ -15,15 +16,15 @@ public class BillboardScript : MonoBehaviour
             switch(billboardAxis)
             {
                 case PotentialAxes.RedAxis:
-                    transform.right = Camera.main.transform.forward;
+                    transform.right = Camera.main.transform.forward * (invertAxis? -1 : 1);
                     break;
 
                 case PotentialAxes.GreenAxis:
-                    transform.up = Camera.main.transform.forward;
+                    transform.up = Camera.main.transform.forward * (invertAxis ? -1 : 1);
                     break;
 
                 case PotentialAxes.BlueAxis:
-                    transform.forward = Camera.main.transform.forward;
+                    transform.forward = Camera.main.transform.forward * (invertAxis ? -1 : 1);
                     break;
             }
             
